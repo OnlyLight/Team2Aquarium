bf71feeb6730e7bb27ea6ae9893172b5befb473e
Phong <phongnguyenhh1996@gmail.com>
2018-02-28 03:10:46 +0700

    update search bar

--- Team2Aquarium-ver3/js/chat-onl.js
+++ Team2Aquarium-ver3/js/chat-onl.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
         $(window).scroll(function() {
             if ($(document).scrollTop() > 50) {
                 $("header").addClass("scroll-down");
+                $("header .row").removeClass("py-md-3");
             } else {
                 $("header").removeClass("scroll-down");
+                $("header .row").addClass("py-md-3");
             }
         });
         $("#btn-chat-onl").click(function() {
             $("#chat-onl").toggleClass("open");
+        });
+        $("#search").mouseenter(function(){
+            $("#search form").stop().slideDown('fast');
+        });
+        $("#search").mouseleave(function(){
+            $("#search form").stop().slideUp('fast');
         });
     });

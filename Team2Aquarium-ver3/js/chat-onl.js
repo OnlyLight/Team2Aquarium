6151889b0f4b9a380b8ed3f4d3394854f6b9c610
Phong <phongnguyenhh1996@gmail.com>
2018-03-02 18:25:12 +0700

    Revert "Edit By Duy 22:02 28/2/2018"
    This reverts commit fec35aa82a3ed476201e5ab86b17bdbfbddb7197.

--- Team2Aquarium-ver3/js/chat-onl.js
+++ Team2Aquarium-ver3/js/chat-onl.js
@@ -1,20 +1,21 @@
-jQuery.scrollSpeed(100, 800);
-
 $(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
-            $("header").addClass("scroll-down");
-        } else {
-            $("header").removeClass("scroll-down");
-        }
+        jQuery.scrollSpeed(100, 800);
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $("header").addClass("scroll-down");
+                $("header .row").removeClass("py-md-3");
+            } else {
+                $("header").removeClass("scroll-down");
+                $("header .row").addClass("py-md-3");
+            }
+        });
+        $("#btn-chat-onl").click(function() {
+            $("#chat-onl").toggleClass("open");
+        });
+        $("#search").mouseenter(function(){
+            $("#search form").stop().slideDown('fast');
+        });
+        $("#search").mouseleave(function(){
+            $("#search form").stop().slideUp('fast');
+        });
     });
-    $("#btn-chat-onl").click(function() {
-        $("#chat-onl").toggleClass("open");
-    });
-    $("#search").mouseenter(function(){
-        $("#search form").stop().slideDown('fast');
-    });
-    $("#search").mouseleave(function(){
-        $("#search form").stop().slideUp('fast');
-    });
-});
